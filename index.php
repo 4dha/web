c7e813b5eba68b8a2af64497d56bdd3318108a57
unknown <4dhamaulana@gmail.com>
2018-05-23 02:13:50 -0700

    file index

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+    
+</head>
+<body>
+<nav>
+        <div class="nav-wrapper">
+           
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+                <i class="material-icons">menu</i>
+            </a>
+            <ul class="right hide-on-med-and-down">
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+               
+                <li>
+                    <a href="login.php">Login</a>
+                </li>
+                <li>
+                    <a href="daftar.php" class="">Daftar</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+    <div class="row">
+                  <div class="col-md-8 col-md-4">
+                      <h1>Docker concepts</h1>
+                      <p class="text-justify">
+                          <b>Docker</b>  is a platform for developers and sysadmins to develop, deploy, and run applications with containers. The use of Linux containers to deploy applications is called containerization. Containers are not new, but their use for easily deploying applications is. 
+                      </p>
+                      <a href="https://docs.docker.com/get-started/" class="btn btn-primary btn-sm">DETAIL</a>
+                      <hr>
+                    </div>
+                   
+                    <div class="col-md-4 ">
+                    <br>
+                   
+                  </div>
+                  
+</div>
+
+                  
+ <div class="row">
+                  <div class="col-md-8 col-sm-6">
+                      <h1>Ubuntu 18.04 LTS out now</h1>
+                      <p class="text-justify">
+                          <b>Ubuntu </b> is an open source software operating system that runs from the desktop, to the cloud, to all your internet connected things
+ 
+                      </p>
+                      <a href="https://www.ubuntu.com/" class="btn btn-primary btn-sm">DETAIL</a>
+                  </div>
+</div>
+                  <hr>
+                 
+                  </div>
+                  <footer class="page-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">PRODI S-1 INFORMATIKA</h5>
+                    <p class="grey-text text-lighten-4">UNIVERSITAS TEKNOLOGI YOGYAKARTA</p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Anggota Kelompok</h5>
+                    <ul>
+                        <li>
+                            Adha Maulana     5150411340
+                        </li>
+                        <li>
+                            Affy Alqorna     5150411327
+                        </li>
+                        <li>
+                            Deby Cahya       5150411328
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                © 2018 Copyright Text
+                
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ kon.php
@@ -0,0 +1,8 @@
+<?php
+    $h='localhost';
+    $u='root';
+    $p='';
+    $d='db_artikel';
+
+    $koneksi=mysqli_connect($h,$u,$p,$d);
+?>
